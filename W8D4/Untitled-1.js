b5e2ee0c841bbac52627d748b2c188d0f652ebaa
wtdavis <wtdavis@wesleyan.edu>
2023-03-02 13:34:53 -0500

    hw

--- W8D4/Untitled-1.js
+++ W8D4/Untitled-1.js
@@ -1,1 +1,7 @@
 // intro JS problems 2 hw
+function jingle(el){ console.log(`Mx. ${el} Jingleheimer Schmidt`)};
+
+function titleize(args, callback) {
+    args.forEach(function(el) {callback(el)} )
+}
+
