ed472bbdcb2657ec04a0c6e9ef834c9278d7b65e
wtdavis <wtdavis@wesleyan.edu>
2023-03-23 00:46:37 -0400

    bedtime

---
+++ W11D4/practice-for-week-15-react-redux-store/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {configureStore} from "./store/index.js"
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import {Provider} from 'react-redux'
+
+const store = configureStore()
+ReactDOM.render(
+  <Provider store={store}>
+  <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
